254d710dbff2199c1bc3bf0544aff1a4d4c2d724
mauro.gomez <gomezmejiam@gmail.com>
2025-08-08 06:56:06 -0500

    actualización de build

---
+++ assets/forward-CCCfqfE-.js
@@ -0,0 +1,6 @@
+import{a}from"./index-C9CIBglf.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15 17 5-5-5-5",key:"nf172w"}],["path",{d:"M4 18v-2a4 4 0 0 1 4-4h12",key:"jmiej9"}]],r=a("forward",o);export{o as __iconNode,r as default};
