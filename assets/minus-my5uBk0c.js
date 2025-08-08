5c98a2d3085a50ba4eb80eaaa1639a37f08bb9c9
mauro.gomez <gomezmejiam@gmail.com>
2025-08-08 18:03:12 -0500

    actualización de build

---
+++ assets/minus-my5uBk0c.js
@@ -0,0 +1,6 @@
+import{a as o}from"./index-DVQc7uBz.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M5 12h14",key:"1ays0h"}]],s=o("minus",a);export{a as __iconNode,s as default};
