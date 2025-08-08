ea660d3d2f08d54f181bb054d19358d8e96c2d46
mauro.gomez <gomezmejiam@gmail.com>
2025-08-08 12:07:29 -0500

    actualización de build

---
+++ assets/chevron-up-D2ATarvU.js
@@ -0,0 +1,6 @@
+import{a as o}from"./index-D4wx5wuv.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],t=o("chevron-up",e);export{e as __iconNode,t as default};
---
+++ assets/list-collapse-DKRbStB2.js
@@ -0,0 +1,6 @@
+import{a}from"./index-D4wx5wuv.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M10 12h11",key:"6m4ad9"}],["path",{d:"M10 18h11",key:"11hvi2"}],["path",{d:"M10 6h11",key:"c7qv1k"}],["path",{d:"m3 10 3-3-3-3",key:"i7pm08"}],["path",{d:"m3 20 3-3-3-3",key:"20gx1n"}]],o=a("list-collapse",e);export{e as __iconNode,o as default};
