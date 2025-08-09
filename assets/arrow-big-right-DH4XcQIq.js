05a8a0c1823e22e2ee2a1259dc71b7059cb6589f
mauro.gomez <gomezmejiam@gmail.com>
2025-08-08 20:31:16 -0500

    actualización de build

---
+++ assets/arrow-big-right-DH4XcQIq.js
@@ -0,0 +1,6 @@
+import{a}from"./index-DBIJmuHw.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M11 9a1 1 0 0 0 1-1V5.061a1 1 0 0 1 1.811-.75l6.836 6.836a1.207 1.207 0 0 1 0 1.707l-6.836 6.835a1 1 0 0 1-1.811-.75V16a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1z",key:"1232du"}]],t=a("arrow-big-right",o);export{o as __iconNode,t as default};
