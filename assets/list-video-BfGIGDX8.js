22d8dd343f2c2e812b0aff76fd6f0f53d53df026
mauro.gomez <gomezmejiam@gmail.com>
2025-08-09 11:28:50 -0500

    actualización de build

---
+++ assets/list-video-BfGIGDX8.js
@@ -0,0 +1,6 @@
+import{a as t}from"./index-kVAB29WI.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M12 12H3",key:"18klou"}],["path",{d:"M12 18H3",key:"11ftsu"}],["path",{d:"M16 6H3",key:"1wxfjs"}],["path",{d:"M21.033 14.44a.647.647 0 0 1 0 1.12l-4.065 2.352a.645.645 0 0 1-.968-.56v-4.704a.645.645 0 0 1 .968-.56z",key:"lh7ho8"}]],o=t("list-video",a);export{a as __iconNode,o as default};
