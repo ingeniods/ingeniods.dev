c784431406debd458a4478e7490b9a82adf008f4
mauro.gomez <gomezmejiam@gmail.com>
2025-08-09 09:53:12 -0500

    actualización de build

---
+++ assets/clock-8-GM8oVYjo.js
@@ -0,0 +1,6 @@
+import{a as c}from"./index-DhHHxqlP.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 6v6l-4 2",key:"imc3wl"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],a=c("clock-8",o);export{o as __iconNode,a as default};
