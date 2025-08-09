4a363cccf59e9dea9385a39bb78892f8b2b7755a
mauro.gomez <gomezmejiam@gmail.com>
2025-08-09 11:26:34 -0500

    actualización de build

---
+++ assets/clock-1-BmZ5AKJx.js
@@ -0,0 +1,6 @@
+import{a as c}from"./index-CDeTZ27c.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 6v6l2-4",key:"miptyd"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],t=c("clock-1",o);export{o as __iconNode,t as default};
