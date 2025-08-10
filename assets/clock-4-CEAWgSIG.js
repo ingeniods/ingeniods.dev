2f7b4b37cc4377cf236dfdd407ed2c79cc0221ec
mauro.gomez <gomezmejiam@gmail.com>
2025-08-10 10:39:51 -0500

    actualización de build

---
+++ assets/clock-4-CEAWgSIG.js
@@ -0,0 +1,6 @@
+import{a as c}from"./index-DIOfa5jI.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],a=c("clock-4",o);export{o as __iconNode,a as default};
