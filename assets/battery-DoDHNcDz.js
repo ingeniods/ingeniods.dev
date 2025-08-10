9ddc926a4e4a7bccf5a185c2f91bce6262633f69
mauro.gomez <gomezmejiam@gmail.com>
2025-08-10 08:57:15 -0500

    actualización de build

---
+++ assets/battery-DoDHNcDz.js
@@ -0,0 +1,6 @@
+import{a as t}from"./index-DcU2AigR.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M 22 14 L 22 10",key:"nqc4tb"}],["rect",{x:"2",y:"6",width:"16",height:"12",rx:"2",key:"13zb55"}]],c=t("battery",e);export{e as __iconNode,c as default};
---
+++ assets/reply-rHsSDh-l.js
@@ -0,0 +1,6 @@
+import{a as e}from"./index-DcU2AigR.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M20 18v-2a4 4 0 0 0-4-4H4",key:"5vmcpk"}],["path",{d:"m9 17-5-5 5-5",key:"nvlc11"}]],o=e("reply",a);export{a as __iconNode,o as default};
