b8f026c01c37fb33eae06fa44700b496f618dd3d
mauro.gomez <gomezmejiam@gmail.com>
2025-08-25 03:41:58 -0500

    actualización de build

---
+++ assets/cloud-check-DYYiRFir.js
@@ -0,0 +1,6 @@
+import{a as o}from"./index-UWomgQ9T.js";/**
+ * @license lucide-react v0.536.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m17 15-5.5 5.5L9 18",key:"15q87x"}],["path",{d:"M5 17.743A7 7 0 1 1 15.71 10h1.79a4.5 4.5 0 0 1 1.5 8.742",key:"9ho6ki"}]],a=o("cloud-check",c);export{c as __iconNode,a as default};
